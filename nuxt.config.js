bdbb2ddc8daf13af65bf7f1d86ab38ffb283be8c
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2021-03-08 22:23:31 +0000

    it works

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,91 +1,84 @@
-
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  ssr: false,
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://buefy.github.io/#/documentation
-    'nuxt-buefy',
+    "nuxt-buefy",
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // Doc: https://auth.nuxtjs.org/guide/setup.html
-    '@nuxtjs/auth',
+    "@nuxtjs/auth"
   ],
   auth: {
     strategies: {
       oauth2: {
-        _scheme: 'oauth2',
-        authorization_endpoint: 'http://localhost:4000/oauth/authorize',
-        userinfo_endpoint: 'http://localhost:4000/oauth/userinfo',
-        scope: ['profile'],
-        access_token_endpoint: 'http://localhost:4000/oauth/token',
-        access_type: 'offline',
-        response_type: 'code',
-        token_type: 'Bearer',
-        client_id: '38a6c291cd9b1d23f5eee131b9c67c41',
-        token_key: 'access_token',
-        grant_type: 'authorization_code',
+        _scheme: "oauth2",
+        authorization_endpoint:
+          "https://shopa-auth.herokuapp.com/oauth/authorize",
+        userinfo_endpoint: "https://shopa-auth.herokuapp.com/oauth/userinfo",
+        scope: ["profile"],
+        access_token_endpoint: "https://shopa-auth.herokuapp.com/oauth/token",
+        client_id: "67d3c69793457a5e0b54102d51631989"
       }
     }
   },
   router: {
-    middleware: ['auth']
+    middleware: ["auth"]
   },
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {},
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
-}
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {}
+};
